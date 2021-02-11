6ea7d73788f332f6dbc913bfb430f1f8f92208a6
Jonah Shin <jonahshin@Jonahs-MacBook-Pro.local>
2021-02-10 17:27:17 -0700

    Added photo dynamic content

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
+    imgs = ["grinnell_1.jpg", "grinnell_2.jpg", "grinnell_3.jpg", "grinnell_4.jpg"]
+    @img = imgs.sample
   end
 
-  def help
+  def contact
+    imgs = ["grinnell_1.jpg", "grinnell_2.jpg", "grinnell_3.jpg", "grinnell_4.jpg"]
+    @img = imgs.sample
   end
 
   def about
+    imgs = ["grinnell_1.jpg", "grinnell_2.jpg", "grinnell_3.jpg", "grinnell_4.jpg"]
+    @img = imgs.sample
   end
 end
