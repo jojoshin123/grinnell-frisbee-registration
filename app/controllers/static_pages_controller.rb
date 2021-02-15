f8964d4dae238e93b277466241edcf0de75d2cc4
Jonah Shin <jonahshin@Jonahs-MacBook-Pro.local>
2021-02-15 12:03:41 -0700

    HW 5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,6 +13,7 @@ class StaticPagesController < ApplicationController
   end
 
   private
+  # selectRandomImage defines an array of all images in the app/assets/images directory and selects a random element
   def selectRandomImage
     imgs = ["grinnell_1.jpg", "grinnell_2.jpg", "grinnell_3.jpg", "grinnell_4.jpg"]
     return imgs.sample
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,27 +2,28 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  # all the images in the app/assets/images directory
   imgs = ["grinnell_1.jpg", "grinnell_2.jpg", "grinnell_3.jpg", "grinnell_4.jpg"]
 
   test "get home and check dynamic image" do
     get root_url
     assert_response :success
-    assert_select "img"
-    assert_includes imgs, assigns(:img)
+    assert_select "img" # check img HTML element
+    assert_includes imgs, assigns(:img) #check if @img is a member of the array
   end
 
   test "get contact and check dynamic image" do
     get contact_url
     assert_response :success
-    assert_select "img"
-    assert_includes imgs, assigns(:img)
+    assert_select "img" # check img HTML element
+    assert_includes imgs, assigns(:img) #check if @img is a member of the array
   end
 
   test "get about and check dynamic image" do
     get about_url
     assert_response :success
-    assert_select "img"
-    assert_includes imgs, assigns(:img)
+    assert_select "img" # check img HTML element
+    assert_includes imgs, assigns(:img) #check if @img is a member of the array
   end
 
 end
