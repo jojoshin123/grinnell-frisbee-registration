666da63227d386459e1265c82aa819620cd0030c
Jonah Shin <jonahshin@Jonahs-MacBook-Pro.local>
2021-02-16 22:40:03 -0700

    Changed Contact page to Roster

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   # get 'static_pages/home'
-  get '/contact', to: "static_pages#contact"
+  get '/roster', to: "static_pages#roster"
   get '/about', to: "static_pages#about"
   # get '/captains', to: "captains#captains"
   resources :captains
